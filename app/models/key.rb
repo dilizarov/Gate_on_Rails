7df644a48dddad43b74cf99d681ae1ca183f3dbf
David Ilizarov <dilizarov@gmail.com>
2014-08-20 01:55:34 -0500

    Created Key model

--- app/models/key.rb
+++ app/models/key.rb
@@ -1,4 +1,5 @@
 class Key < ActiveRecord::Base
+  include Externalable
 
   # This is not confusing at all. :)
   attr_encryptor :key,      key: ENV['KEY_KEY']
@@ -6,7 +7,15 @@ class Key < ActiveRecord::Base
 
   validates :gatekeeper_id, presence: true
   validates :networks,      presence: true
-  validates :key,           presence: true
+  validates :key,           presence: true,
                             uniqueness: true
+                            
+  def generate_key!
+    # key is a random 16 digit number
+    loop do
+      key = rand(1_000_000_000_000_000...10_000_000_000_000_000)
+      break self.key = key unless Key.find_by(key: key)
+    end
+  end
   
 end

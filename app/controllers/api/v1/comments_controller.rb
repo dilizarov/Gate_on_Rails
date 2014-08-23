e2d134b7b9863906fed30f16da042bd64a5e43b7
David Ilizarov <dilizarov@gmail.com>
2014-08-23 00:53:12 -0500

    Added very similar REDIS callbacks to the comments model. As it stands, I noticed there is a lot of authorization logic going on, so I'll spend the next commit or few adding CanCan and getting that set-up with all the controllers

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,4 @@
 class Api::V1::CommentsController < ApplicationController
+  
+  
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,22 @@ class Comment < ActiveRecord::Base
   validates :body,        presence: true,
                           length: { maximum: 500 }
   
+  after_create :add_to_post!
+  after_destroy :remove_from_post!
+  
   belongs_to :user
-  belongs_to :post                        
+  belongs_to :post
+  
+  
+  def add_to_post!
+    REDIS.lpush(feed_post_key, self.id)
+  end
+  
+  def remove_from_post!
+    REDIS.lrem(feed_post_key, 1, self.id)
+  end
+  
+  def feed_post_key
+    "post:#{self.post_id}:comments"
+  end                        
 end

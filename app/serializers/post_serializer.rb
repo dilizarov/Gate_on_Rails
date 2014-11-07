d9272e1d3c9570a882be869631b40a0c583f6bd9
David Ilizarov <dilizarov@gmail.com>
2014-11-07 04:39:32 -0800

    Forgot to add in Post Serializer that the Network has a different named serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,5 +2,5 @@ class PostSerializer < ActiveModel::Serializer
   attributes :external_id, :body, :created_at, :comments_count
   
   has_one :user
-  has_one :network
+  has_one :network, serializer: SimpleNetworkSerializer
 end

daf9885b9605273ea2458680e7f8530fe2ed20aa
David Ilizarov <dilizarov@gmail.com>
2014-11-19 11:48:17 -0800

    have env variables substitute passwords, etc.

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: 'Gate',
-  password:  'GateLRRY512',
+  user_name: ENV['SENDGRID_USERNAME'],
+  password:  ENV['SENDGRID_PASSWORD'],
   domain:    'unlockgate.today',
   address:   'smtp.sendgrid.net',
   port:      587,

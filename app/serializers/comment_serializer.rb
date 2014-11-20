f72f009e6c517fd16e536013d97276f056637a30
David Ilizarov <dilizarov@gmail.com>
2014-11-19 19:09:00 -0800

    Added up_count to serializer for both posts and comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,10 +1,14 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :external_id, :body, :created_at
+  attributes :external_id, :body, :created_at, :up_count
   
   def attributes
     data = super
     data[:uped] = object.uped unless object.uped.nil?
     data
+  end
+  
+  def up_count
+    object.cached_votes_up
   end
   
   has_one :user
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,10 +1,14 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :external_id, :body, :created_at, :comments_count
+  attributes :external_id, :body, :created_at, :comments_count, :up_count
   
   def attributes
     data = super
     data[:uped] = object.uped unless object.uped.nil?
     data
+  end
+  
+  def up_count
+    object.cached_votes_up
   end
   
   has_one :user

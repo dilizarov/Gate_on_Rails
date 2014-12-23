2a6dc029c716bd7ccbec21fd300ca5b839c7a4eb
David Ilizarov <dilizarov@gmail.com>
2014-12-23 04:17:23 -0600

    notifications not finished yet so commented out

--- app/workers/Notifications.rb
+++ app/workers/Notifications.rb
@@ -1,13 +1,13 @@
-class Notifications
-  include Sidekiq::Worker
-  
-  def perform
-    destination = ["lel"]
-    
-    data = {
-      :key = "Hello there"
-    }
-    
-    GCM.send_notification(destination, data)
-  end
-end
+# class Notifications
+#   include Sidekiq::Worker
+#
+#   def perform
+#     destination = ["lel"]
+#
+#     data = {
+#       :key = "Hello there"
+#     }
+#
+#     GCM.send_notification(destination, data)
+#   end
+# end

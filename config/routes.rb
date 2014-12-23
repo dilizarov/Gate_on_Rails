c09170a64e15b8dd401dc19b00e4837128b6fa97
David Ilizarov <dilizarov@gmail.com>
2014-12-23 04:50:37 -0600

    forgot post for login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
       devise_for :users, only: :registrations, path: '/registrations'
       
       post   'sessions'      => 'sessions#create',      as: 'login'
-      post   'sessions'      => 'sessions#destroy',     as: 'logout'
+      delete 'sessions'      => 'sessions#destroy',     as: 'logout'
       
       resources :networks, only: [:create, :index, :show] do
         member do

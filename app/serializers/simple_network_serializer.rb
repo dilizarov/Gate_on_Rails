139edd0d76bcd0cb8a398b64d7d62a52ed6c4b71
David Ilizarov <dilizarov@gmail.com>
2015-01-10 23:21:34 -0600

    Also renamed the files

--- app/serializers/simple_network_serializer.rb
+++
@@ -1,9 +0,0 @@
-class SimpleNetworkSerializer < ActiveModel::Serializer
-  attributes :name, :external_id
-  
-  def attributes
-    data = super
-    data[:users_count] = object.users_count unless object.users_count.nil?
-    data
-  end
-end

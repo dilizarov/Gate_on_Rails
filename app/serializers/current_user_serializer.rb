d8c845080c2426be0328e43d1e111c1e9b303c45
David Ilizarov <dilizarov@gmail.com>
2015-01-15 08:39:17 -0500

    And I thought I had the serializer flawless. derp

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -1,3 +1,9 @@
 class CurrentUserSerializer < ActiveModel::Serializer
-  attributes :name, :email, :external_id, :created_at, :auth_token
+  attributes :name, :email, :external_id, :created_at
+  
+  def attributes
+    data = super
+    data[:auth_token] = object.auth_token unless object.auth_token.nil?
+    data
+  end 
 end

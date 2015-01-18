4e7a61303ca0e6e83f62265b0f19b7ad428c657e
David Ilizarov <dilizarov@gmail.com>
2015-01-18 03:00:33 -0600

    Fixed up serializer for readability

--- app/serializers/key_serializer.rb
+++ app/serializers/key_serializer.rb
@@ -1,6 +1,11 @@
 class KeySerializer < ActiveModel::Serializer
   attributes :key, :gates
 
+  def key
+    # 1234567891234567 turns into 1234-5678-9123-4567 for readability
+    object.key.scan(/.{1,4}).join("-")
+  end
+  
   def gates
     keys_gates = Gate.where(id: object.gates).to_a
     ActiveModel::ArraySerializer.new(keys_gates, each_serializer: SimpleGateSerializer)

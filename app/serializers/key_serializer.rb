fb90ae6416205f30fdc3d85fc4bdd252b0799641
David Ilizarov <dilizarov@gmail.com>
2015-01-18 03:14:13 -0600

    have to to_s the serialized key

--- app/serializers/key_serializer.rb
+++ app/serializers/key_serializer.rb
@@ -2,8 +2,8 @@ class KeySerializer < ActiveModel::Serializer
   attributes :key, :gates
 
   def key
-    # 1234567891234567 turns into 1234-5678-9123-4567 for readability
-    object.key.scan(/.{1,4}/).join("-")
+    # 1234567891234567 turns into "1234-5678-9123-4567" for readability
+    object.key.to_s.scan(/.{1,4}/).join("-")
   end
   
   def gates

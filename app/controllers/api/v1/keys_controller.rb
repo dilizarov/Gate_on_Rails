5cbe6f18b64a781d81f0e49351ca32dbbe47f009
David Ilizarov <dilizarov@gmail.com>
2015-01-18 23:58:51 -0600

    raise from not throw

--- app/controllers/api/v1/keys_controller.rb
+++ app/controllers/api/v1/keys_controller.rb
@@ -35,7 +35,7 @@ class Api::V1::KeysController < ApiController
     @key = Key.find_by_key(params[:id])
     
     if @key && @key.active?
-      throw CanCan::AccessDenied if @key.gatekeeper_id == current_user.id
+      rescue CanCan::AccessDenied if @key.gatekeeper_id == current_user.id
       
       @new_gates = @key.process(current_user)
       @new_gates = current_user.gates_with_users_count(includes: :creator).select do |gate|

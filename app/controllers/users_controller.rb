92162d73ebd81dcfc3bb1597ade6512fc80d6f97
David Ilizarov <dilizarov@gmail.com>
2015-01-20 06:13:34 -0600

    Checking to see if its because im in the public index html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      if: Proc.new { |c| c.request.format == 'application/json' },
                      only: :reset_password
+                     
+  def index
+  end
   
   def send_forgot_password_email
     @user = User.find_by(email: params[:email])

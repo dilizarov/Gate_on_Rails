0947600197198e5c378cdbc6d5450da9142f4102
David Ilizarov <dilizarov@gmail.com>
2015-02-19 21:45:01 -0600

    Testing notif stuff and need debugger

--- config/initializers/apple_push_notifications.rb
+++ config/initializers/apple_push_notifications.rb
@@ -1,4 +1,4 @@
-APNS.host = 'gateway.push.apple.com'
+APNS.host = 'gateway.sandbox.push.apple.com'
 APNS.port = 2195
-APNS.pem = "#{Rails.root}/config/GateProductionCert.pem"
+APNS.pem = "#{Rails.root}/config/GateiOSDevCertificate.pem"
 APNS.pass = ''
